75769863cf9eb6de7325bb7b98529e43132e2a8d
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-01-25 21:28:47 +0100

    Init commit

---
+++ src/app/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import style from './sass/style.scss';
+import createStore from './stores/index.js';
+import reducer from './reducers/index.js';
+import App from './components/App.jsx';
+
+var store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
