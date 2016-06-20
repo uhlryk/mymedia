a667705b3dc5250dac3e244e106cd37373cd0f60
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-06-20 22:26:10 +0200

    prepare form

--- app/routes/project/media/EditFile.jsx
+++ app/routes/project/media/EditFile.jsx
@@ -4,28 +4,59 @@ import { connect } from "react-redux";
 import { push } from "react-router-redux";
 
 @connect(state => ({
+  fileList: state.fileList
 }))
 class EditFile extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      details: this.props.fileList[this.props.params.hashPath],
+      hashPath: this.props.params.hashPath,
+      validation: {}
+    };
     this.onCloseClick = this.onCloseClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onCloseClick() {
     this.props.dispatch(push("project/media"));
   }
 
+  handleNameChange(evt) {
+    this.setState({
+      details: Object.assign({}, this.state.details, { email: evt.target.value})
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    if(this.validation() === false) {
+      return;
+    }
+    //dispatch to reducer
+  }
+
   render() {
-    console.log(this.props);
+    console.log(this.state);
     return (
       <div className="edit-form">
-        <div>
-          {this.props.params.hashPath}
-        </div>
-        <div>
+        <form onSubmit={this.handleSubmit}>
+          <RB.Input
+            type='text'
+            value={this.state.details.name}
+            placeholder='Enter name'
+            label='Name'
+            ref='nameInput'
+            help={this.state.validation.name}
+            groupClassName='group-class'
+            labelClassName='label-class'
+            onChange={this.handleNameChange} />
+
+          <RB.ButtonInput type='submit' value='Submit Button' />
           <RB.Button bsStyle="primary" onClick={this.onCloseClick}>Close</RB.Button>
-        </div>
+        </form>
       </div>
     );
   }
