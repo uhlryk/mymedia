6a8d6bcca5cabcaedc2aa33a5aa615f79bfc44aa
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-12-08 18:44:27 +0100

    grid row toggle short long

--- app/components/grid/CustomRow.jsx
+++ app/components/grid/CustomRow.jsx
@@ -11,30 +11,42 @@ class CustomRow extends React.Component {
 
   constructor(props) {
     super(props);
-    this.onEditClick = this.onEditClick.bind(this);
+    this.state = {
+      short: true
+    }
+    this.onToggleSize = this.onToggleSize.bind(this);
   }
 
-  onEditClick() {
-    this.props.dispatch(push("project/media/edit-file/" + this.props.data.hashPath));
+  onToggleSize() {
+    this.setState({
+      short: !this.state.short
+    })
   }
 
   render() {
-
+    let toogleSizeLabel = "More";
+    let rowClassName = "list__row";
+    if (!this.state.short) {
+      toogleSizeLabel = "Less";
+      rowClassName += " list__row--long";
+    }
     return (
-      <div className="list__row">
-        <div className="list__name">{this.props.data.name}</div>
+      <div className={rowClassName}>
+        <div className="list__name" onClick={this.onStartEditClick}>{this.props.data.name}</div>
         <div className="list__original-path">{this.props.data.path}</div>
-        <div className="list__size">
-          <FileSize data={this.props.data.size} />
-        </div>
-        <div className="list__birthtime">
-          <DateDisplay data={this.props.data.birthtime} />
-        </div>
-        <div className="list__description">
-          <DateDisplay data={this.props.data.description} />
+        <div className="list__additional">
+          <div className="list__size">
+            <FileSize data={this.props.data.size} />
+          </div>
+          <div className="list__birthtime">
+            <DateDisplay data={this.props.data.birthtime} />
+          </div>
+          <div className="list__description">
+            <DateDisplay data={this.props.data.description} />
+          </div>
         </div>
         <div className="list__actions">
-          <RB.Button bsStyle="primary" onClick={this.onEditClick} >Edit</RB.Button>
+          <p bsStyle="primary" onClick={this.onToggleSize} >{toogleSizeLabel}</p>
         </div>
       </div>
     );
