0846ff9c9d4d8a01c757bca2ce30173536a8e2d7
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-05-10 23:27:32 +0200

    #136 update compare number

--- app/features/attributes/fileSize/index.js
+++ app/features/attributes/fileSize/index.js
@@ -8,19 +8,4 @@ export default class FileSizeAttributesExtension extends NumberAttributesExtensi
     this.setView(View);
     this.setOnlyProjectExtensionUse();
   }
-
-  getSortFunction (attributeId, order) {
-    return (a, b) => {
-      const aNum = Number(a[attributeId]);
-      const bNum = Number(b[attributeId]);
-      if (order === "ASC") {
-        if (aNum < bNum) return -1;
-        if (aNum > bNum) return 1;
-      } else {
-        if (aNum < bNum) return 1;
-        if (aNum > bNum) return -1;
-      }
-      return 0;
-    };
-  }
 }
--- app/features/attributes/number/index.js
+++ app/features/attributes/number/index.js
@@ -6,4 +6,16 @@ export default class NumberAttributesExtension extends InputAttributesExtension 
     this.setName ("number");
     this.setDisplayName("number");
   }
+
+  getSortFunction (attributeId, order) {
+    return (a, b) => {
+      const aNum = Number(a[attributeId]);
+      const bNum = Number(b[attributeId]);
+      if (order === "ASC") {
+        return aNum - bNum;
+      } else {
+        return bNum - aNum;
+      }
+    };
+  }
 }
