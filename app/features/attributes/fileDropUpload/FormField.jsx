c4a4c4057a7d1847cc82805d7ed3d2ba0eb6ed3c
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-06-27 21:04:02 +0200

    remove existing files; recreate files

--- app/features/attributes/fileDropUpload/FormField.jsx
+++ app/features/attributes/fileDropUpload/FormField.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import md5 from "md5";
+import uuid from "uuid-v4";
 import DropFile from "./DropFile";
 import FileDropUploadAttributesExtension from "./index";
 
@@ -14,11 +14,42 @@ export default class FormField extends React.Component {
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
+    this.onRemoveClick = this.onRemoveClick.bind(this);
+    this.onRecreateClick = this.onRecreateClick.bind(this);
+  }
+
+  onRecreateClick(fileId) {
+    this.props.onChange(this.props.value
+      .map(fileData => {
+        if (fileData.id === fileId && fileData.status === FileDropUploadAttributesExtension.fileStatus.BEFORE_DELETE) {
+          return Object.assign({}, fileData, {
+            status: FileDropUploadAttributesExtension.fileStatus.UPLOADED
+          });
+        } else {
+          return fileData
+        }
+      }));
+  }
+
+  onRemoveClick(fileId) {
+    this.props.onChange(this.props.value
+      .filter(fileData => {
+        return !(fileData.id === fileId && fileData.status === FileDropUploadAttributesExtension.fileStatus.BEFORE_UPLOAD)
+      })
+      .map(fileData => {
+        if (fileData.id === fileId && fileData.status === FileDropUploadAttributesExtension.fileStatus.UPLOADED) {
+          return Object.assign({}, fileData, {
+            status: FileDropUploadAttributesExtension.fileStatus.BEFORE_DELETE
+          });
+        } else {
+          return fileData
+        }
+      }));
   }
 
   onChange(files) {
-    console.log(files);
     const newValue = (this.props.value || []).concat(Array.from(files).map(file => ({
+      id: uuid(),
       file,
       status: FileDropUploadAttributesExtension.fileStatus.BEFORE_UPLOAD
     })));
@@ -26,17 +57,26 @@ export default class FormField extends React.Component {
   }
 
   render() {
+    const fileList = (this.props.value || []).map(fileData => {
+      let button;
+      if (fileData.status === FileDropUploadAttributesExtension.fileStatus.BEFORE_DELETE) {
+        button = <button type="button" className="button button--warning" onClick={() => this.onRecreateClick(fileData.id)} >Recreate</button>;
+      } else {
+        button = <button type="button" className="button button--danger" onClick={() => this.onRemoveClick(fileData.id)} >Remove</button>;
+      }
+      return (
+        <tr key={fileData.id}>
+          <td>{fileData.name || fileData.file.name}</td>
+          <td>{fileData.status}</td>
+          <td>{button}</td>
+        </tr>
+      )
+    });
     return (
       <div>
         <table className="table table-striped table-condensed table-hover">
           <tbody>
-          {(this.props.value || []).map(fileData => (
-            <tr key={fileData.id || md5(fileData.file.path)}>
-              <td>{fileData.name || fileData.file.name}</td>
-              <td>{fileData.status}</td>
-              <td><button className="button button--danger">Remove</button></td>
-            </tr>
-          ))}
+          {fileList}
           </tbody>
         </table>
         <DropFile
--- app/features/attributes/fileDropUpload/index.js
+++ app/features/attributes/fileDropUpload/index.js
@@ -2,7 +2,7 @@ import Settings from "./Settings.jsx";
 import FormField from "./FormField.jsx";
 import View from "./View.jsx";
 import AttributesExtension from "../AttributesExtension";
-import uuid from "uuid-v4";
+
 import path from "path";
 import fse from "fs-extra";
 
@@ -41,15 +41,17 @@ export default class FileDropUploadAttributesExtension extends AttributesExtensi
           const projectPath = project.path;
           const resourcePath = path.join(projectPath, resource.id);
           const srcFilePath = file.path;
-          const fileId = uuid();
-          const targetFilePath = path.join(resourcePath, fileId);
+
+          const targetFilePath = path.join(resourcePath, fileData.id);
           await fse.copy(srcFilePath, targetFilePath);
           newFiles.push({
-            id: fileId,
+            id: fileData.id,
             name: file.name,
             path: targetFilePath,
             status: FileDropUploadAttributesExtension.fileStatus.UPLOADED
           });
+        } else if(fileData.status === FileDropUploadAttributesExtension.fileStatus.BEFORE_DELETE) {
+          await fse.remove(fileData.path);
         } else {
           newFiles.push(fileData);
         }
