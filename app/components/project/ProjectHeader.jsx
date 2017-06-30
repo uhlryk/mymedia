912279cd3c044bd037a16131d9e3188c1494219c
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-07-01 00:03:38 +0200

    Header component

--- app/components/project/ProjectHeader.jsx
+++ app/components/project/ProjectHeader.jsx
@@ -5,6 +5,7 @@ import ManageCreate from "../attributes/ManageCreate.jsx";
 import classNames from "classnames";
 import { push } from "react-router-redux";
 import ManageForm from "../resources/ManageForm.jsx";
+import Header from "../Header.jsx";
 
 @connect(state => ({
   project: state.project
@@ -73,34 +74,20 @@ class ProjectHeader extends React.Component {
     });
 
     return (
-      <nav className="project-header">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">{this.props.project.name}</a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="#" onClick={ this.onAddNewClick }>Create resource</a></li>
-              <li className={menuClassName}>
-                <a href="#" className="dropdown-toggle" onClick={this.onMenuClick} data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Project details</a></li>
-                  <li><a href="#" onClick={this.onManageClick}>Attributes</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#" onClick={() => this.props.dispatch(push("project/menu"))}>Project List</a></li>
-                </ul>
-              </li>
+      <Header branding={this.props.project.name}>
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#" onClick={ this.onAddNewClick }>Create resource</a></li>
+          <li className={menuClassName}>
+            <a href="#" className="dropdown-toggle" onClick={this.onMenuClick} data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span className="caret"></span></a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Project details</a></li>
+              <li><a href="#" onClick={this.onManageClick}>Attributes</a></li>
+              <li role="separator" className="divider"></li>
+              <li><a href="#" onClick={() => this.props.dispatch(push("project/menu"))}>Project List</a></li>
             </ul>
-          </div>
-        </div>
-      </nav>
+          </li>
+        </ul>
+      </Header>
     );
   }
 }
