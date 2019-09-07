bfdfbd3a4bade2efd4618ab50b981b116ee9d366
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-09-07 23:18:10 +0200

    open file from details

--- src/app/modules/details/details.component.ts
+++ src/app/modules/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { ProjectContextService } from "../../services/projectContext.service";
+import { ActivatedRoute } from "@angular/router";
+const {shell} = (<any>window).require("electron");
+import * as path from "path";
+import File from "../../types/File";
 
 @Component({
     selector: "app-file",
@@ -6,7 +11,20 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./details.component.scss"]
 })
 export class DetailsComponent implements OnInit {
-    constructor() {}
+    file: File;
+    constructor(
+        private projectContextService: ProjectContextService,
+        private route: ActivatedRoute
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            const fileId: string = params.get("fileId");
+            this.file = this.projectContextService.getFile(fileId);
+        });
+    }
+
+    openFile() {
+        shell.openItem(path.join(this.projectContextService.getProjectPath(), this.file.filePath));
+    }
 }
