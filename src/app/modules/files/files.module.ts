1a3f7f832cb02a05faa86520e436f337c3fbe5e9
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-09-10 21:23:39 +0200

    row is a component

--- src/app/modules/files/files.module.ts
+++ src/app/modules/files/files.module.ts
@@ -1,10 +1,10 @@
-
 import { NgModule } from "@angular/core";
-import { CommonModule} from '@angular/common';
-import {FilesComponent} from "./files.component";
+import { CommonModule } from "@angular/common";
+import { FilesComponent } from "./files.component";
+import { RowComponent } from "./row/row.component";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [FilesComponent]
+    imports: [CommonModule],
+    declarations: [FilesComponent, RowComponent]
 })
 export class FilesModule {}
---
+++ src/app/modules/files/row/row.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, OnInit} from "@angular/core";
+import File from "../../../types/File";
+
+@Component({
+    selector: "[app-row]",
+    templateUrl: "./row.component.html",
+    styleUrls: ["./row.component.scss"]
+})
+export class RowComponent implements OnInit {
+    @Input() file: File;
+    constructor() {}
+
+    ngOnInit() {}
+}
