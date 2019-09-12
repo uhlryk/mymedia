2b42201f5f20ccff3a741a88ab28733d9ba31431
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-09-12 22:29:13 +0200

    search files by name and tags

--- src/app/modules/files/files.component.ts
+++ src/app/modules/files/files.component.ts
@@ -8,23 +8,40 @@ import { Router } from "@angular/router";
 })
 export class FilesComponent implements OnInit {
     fileList;
+    searchInput;
     constructor(
         private projectContextService: ProjectContextService,
         private router: Router
     ) {}
     ngOnInit() {
-        const files = this.projectContextService.getFiles();
-        this.fileList = files;
+        this.fileList = this.projectContextService.getFiles();
     }
 
     openFile(fileId) {
-        this.projectContextService.openFile(
-            fileId
-        );
+        this.projectContextService.openFile(fileId);
     }
 
     showFileDetails(fileId) {
-        console.log(fileId);
         this.router.navigate([`/files/${fileId}`]);
+    }
+
+    startSearch() {
+        const projectTags = this.projectContextService
+            .getTags()
+            .filter(tag => tag.name.toLowerCase().includes(this.searchInput.toLowerCase()));
+        this.fileList = this.projectContextService.getFiles().filter(file => {
+            if (file.newFileName.toLowerCase().includes(this.searchInput.toLowerCase())) {
+                return true;
+            }
+            const matchedFileTags = file.tags.filter(fileTagId =>
+                !!projectTags.find(tag => (tag.id === fileTagId))
+            );
+            if (
+                matchedFileTags.length > 0
+            ) {
+                return true;
+            }
+        });
+
     }
 }
--- src/app/modules/files/files.module.ts
+++ src/app/modules/files/files.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FilesComponent } from "./files.component";
 import { RowComponent } from "./row/row.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     declarations: [FilesComponent, RowComponent]
 })
 export class FilesModule {}
