ef76e0e2134084c300ef0f2bad721f5d6548e1bf
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-09-15 23:21:26 +0200

    new project filter out hidden/removed files

--- src/app/services/getFileList.ts
+++ src/app/services/getFileList.ts
@@ -2,23 +2,25 @@ import fse from "fs-extra";
 import path from "path";
 
 async function walk(baseDir, dir) {
-  let results = [];
-  let fileList = await fse.readdir(dir);
-  for (let fileName of fileList) {
-    let filePath = path.resolve(dir, fileName);
-    let fileStat = await fse.stat(filePath);
-    if (fileStat && fileStat.isDirectory()) {
-      Array.prototype.push.apply(results, await walk(baseDir, filePath));
-    } else if (fileStat && fileStat.isFile()){
-      results.push({
-        name: fileName,
-        path: path.relative(baseDir, filePath),
-        stat: fileStat
-      });
+    let results = [];
+    let fileList = await fse.readdir(dir);
+    for (let fileName of fileList) {
+        let filePath = path.resolve(dir, fileName);
+        let fileStat = await fse.stat(filePath);
+        if (fileStat && fileStat.isDirectory()) {
+            Array.prototype.push.apply(results, await walk(baseDir, filePath));
+        } else if (fileStat && fileStat.isFile()) {
+            if (!/(^|\/)\.[^\/\.]/g.test(filePath)) {
+                results.push({
+                    name: fileName,
+                    path: path.relative(baseDir, filePath),
+                    stat: fileStat
+                });
+            }
+        }
     }
-  }
-  return results;
+    return results;
 }
 export default async function getFileList(dir) {
-  return await walk(dir, dir);
+    return await walk(dir, dir);
 }
