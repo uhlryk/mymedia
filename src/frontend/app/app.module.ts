39ae8566c2cce75b4f83b8605c86cb28586dabd8
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-10-13 21:32:00 +0200

    material top menu

--- src/frontend/app/app.module.ts
+++ src/frontend/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { TopMenuComponent } from "./components/top-menu/top-menu.component";
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, TopMenuComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatToolbarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
