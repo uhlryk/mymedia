48dbba84976af5f5d9f3633f7a33c5391cb7e6ab
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-11-24 00:04:22 +0100

    remove old top menu

--- src/frontend/app/app.module.ts
+++ src/frontend/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { TabMenuModule } from "primeng/tabmenu";
-import { TopMenuComponent } from "./components/top-menu/top-menu.component";
 import { LoaderComponent } from "./components/loader/loader.component";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
 import { ProgressBarModule } from "primeng/progressbar";
 import { LoaderService } from "./services/loader.service";
 @NgModule({
-    declarations: [AppComponent, TopMenuComponent, LoaderComponent],
+    declarations: [AppComponent, LoaderComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
