cab25070957ec736d48d12b3907165bc46fe1c34
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-07 23:13:16 +0100

    style dropdown

--- src/frontend/app/modules/files/modules/tag/tag.module.ts
+++ src/frontend/app/modules/files/modules/tag/tag.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from "@angular/common";
 import { TagComponent } from "./components/tag/tag.component";
 import { ButtonModule } from "primeng/button";
 import { TooltipModule } from "primeng/tooltip";
-import { DropdownModule } from "primeng/dropdown";
+
 @NgModule({
     declarations: [TagComponent],
-    imports: [CommonModule, ButtonModule, TooltipModule, DropdownModule],
+    imports: [CommonModule, ButtonModule, TooltipModule],
     exports: [TagComponent]
 })
 export class TagModule {}
