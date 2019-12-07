577e4a4ec86913a8593022e6aeecdcd137c977c9
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-07 23:23:04 +0100

    style tags in row list

--- src/frontend/app/modules/files/components/row/row.component.ts
+++ src/frontend/app/modules/files/components/row/row.component.ts
@@ -1,19 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from "@angular/core";
 import ResourceModel from "../../../../models/resource.model";
+import TagModel from "../../../../models/tag.model";
 
 @Component({
     selector: "app-row",
     templateUrl: "./row.component.html",
     styleUrls: ["./row.component.scss"]
 })
-export class RowComponent implements OnInit {
+export class RowComponent implements OnInit, OnChanges {
     @Input() resource: ResourceModel;
     @Output() openDetailsModal = new EventEmitter<string>();
     @Output() executeMedia = new EventEmitter<string>();
+    private _tagList: Array<TagModel>;
     constructor() {}
 
     ngOnInit() {}
 
+    ngOnChanges() {
+        this._tagList = this.resource.getResourceTagModelList().slice(0, 4);
+    }
     clickOpenDetails() {
         this.openDetailsModal.emit(this.resource.getId());
     }
