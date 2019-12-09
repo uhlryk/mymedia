88105409ad6919ba980e3039b8684e4564588e76
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-09 22:59:32 +0100

    show tags in more menu

--- src/frontend/app/modules/files/modules/tag-list/tag-list.module.ts
+++ src/frontend/app/modules/files/modules/tag-list/tag-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { TagListComponent } from "./components/tag-list/tag-list.component";
 import { TagModule } from "../tag/tag.module";
 import { ButtonModule } from "primeng/button";
+import { OverlayPanelModule } from "primeng/overlaypanel";
 @NgModule({
     declarations: [TagListComponent],
-    imports: [CommonModule, TagModule, ButtonModule],
+    imports: [CommonModule, TagModule, ButtonModule, OverlayPanelModule],
     exports: [TagListComponent]
 })
 export class TagListModule {}
