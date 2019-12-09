75aa355a8541c3d1b4a38860c4fa92a770d39314
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-09 21:38:28 +0100

    add more button

--- src/frontend/app/modules/files/modules/tag-list/tag-list.module.ts
+++ src/frontend/app/modules/files/modules/tag-list/tag-list.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TagListComponent } from "./components/tag-list/tag-list.component";
 import { TagModule } from "../tag/tag.module";
-
+import { ButtonModule } from "primeng/button";
 @NgModule({
     declarations: [TagListComponent],
-    imports: [CommonModule, TagModule],
+    imports: [CommonModule, TagModule, ButtonModule],
     exports: [TagListComponent]
 })
 export class TagListModule {}
