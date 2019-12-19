4c161a46ec40630a33a02bbb09e4deb8e15d70d1
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-19 22:15:16 +0100

    tag-list accept pure input

--- src/frontend/app/modules/files/modules/card/components/card/card.component.ts
+++ src/frontend/app/modules/files/modules/card/components/card/card.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import ResourceModel from "../../../../../../models/resource.model";
 import TagModel from "../../../../../../models/tag.model";
-
+import { TagModel as TagComponentModel } from "../../../tag-list/components/tag-list/tag-list.component";
 @Component({
     selector: "app-card",
     templateUrl: "./card.component.html",
@@ -11,11 +11,9 @@ export class CardComponent implements OnInit {
     @Input() resource: ResourceModel;
     @Output() openDetailsModal = new EventEmitter<string>();
     @Output() executeMedia = new EventEmitter<string>();
-    _tagList: Array<TagModel>;
     constructor() {}
 
     ngOnInit() {
-        this._tagList = this.resource.getResourceTagModelList();
     }
 
     clickOpenDetails() {
@@ -24,5 +22,12 @@ export class CardComponent implements OnInit {
 
     clickExecuteMedia() {
         this.executeMedia.emit(this.resource.getId());
+    }
+
+    get tagList(): Array<TagComponentModel> {
+        return this.resource.getResourceTagModelList().map((tagModel: TagModel) => ({
+            id: tagModel.getId(),
+            name: tagModel.getName()
+        }));
     }
 }
--- src/frontend/app/modules/files/modules/tag-list/components/tag-list/tag-list.component.ts
+++ src/frontend/app/modules/files/modules/tag-list/components/tag-list/tag-list.component.ts
@@ -1,19 +1,20 @@
 import { Component, Input, OnChanges, OnInit } from "@angular/core";
-import TagModel from "../../../../../../models/tag.model";
 
 @Component({
     selector: "app-tag-list",
     templateUrl: "./tag-list.component.html",
     styleUrls: ["./tag-list.component.scss"]
 })
-export class TagListComponent implements OnInit, OnChanges {
-    @Input() tags: Array<TagModel>;
-    private _lineTagList: Array<TagModel>;
+export class TagListComponent {
+    @Input("tag-list") _tagList: Array<TagModel>;
     constructor() {}
 
-    ngOnInit() {}
-
-    ngOnChanges() {
-        this._lineTagList = this.tags;
+    get tagList(): Array<TagModel> {
+        return this._tagList;
     }
+}
+
+export interface TagModel {
+    id: string;
+    name: string;
 }
