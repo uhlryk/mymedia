3159955b726805bdc38103897be40d7a558fcb3e
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-12-23 17:53:09 +0100

    simplify tag-list

--- src/frontend/app/modules/files/modules/tag-list/components/tag-list/tag-list.component.ts
+++ src/frontend/app/modules/files/modules/tag-list/components/tag-list/tag-list.component.ts
@@ -6,16 +6,10 @@ import { Component, Input, OnChanges, OnInit } from "@angular/core";
     styleUrls: ["./tag-list.component.scss"]
 })
 export class TagListComponent {
-    @Input("tagList") _tagList: Array<{
+    @Input("tagList") tagList: Array<{
         id: string;
         name: string;
     }>;
     constructor() {}
 
-    get tagList(): Array<{
-        id: string;
-        name: string;
-    }> {
-        return this._tagList || [];
-    }
 }
